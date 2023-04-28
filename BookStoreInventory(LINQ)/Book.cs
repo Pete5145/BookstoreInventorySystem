5fb7001730e73b3b10b5802958855db082f0f8e3
Pete5145 <peterosakpolor661@gmail.com>
2023-04-28 12:32:25 +0100

    Add project files.

---
+++ BookStoreInventory(LINQ)/Book.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Xsl;
+
+namespace BookStoreInventory_LINQ_
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+
+        public override string ToString()
+        {
+            return $"Title: {Title}\nAuthor: {Author}\nPrice: {Price}\nStockQuantity: {StockQuantity}";
+        }
+
+        public static void ViewAllBooks(IList<Book> books)
+        {
+            var allBooks = books.Select(x => x);
+            foreach (var item in allBooks)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine("============================================================");
+            }
+        }
+
+        public static void ViewBooksByAuthor(IList<Book> books, string author)
+        {
+            var booksByAuthor  = books.Where(x => author == x.Author);
+            foreach (var item in booksByAuthor)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine("============================================================");
+            }
+        }
+
+        public static void ViewBooksByPrice(IList<Book> books, decimal minPrice, decimal maxPrice)
+        {
+            var booksByPrice = books.Where(x => x.Price > minPrice && x.Price < maxPrice);
+            foreach (var item in booksByPrice)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine("============================================================");
+            }
+        }
+
+        public static void ViewBooksOutOfStock(IList<Book> books)
+        {
+            var booksOutOfStock = books.Where(x => x.StockQuantity == 0);
+            foreach (var item in booksOutOfStock)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine("============================================================");
+            }
+        }
+
+    }
+}
---
+++ BookStoreInventory(LINQ)/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace BookStoreInventory_LINQ_
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Book> books = new List<Book>()
+            {
+                new Book
+                {
+                    Title = "Crime and Punishment",
+                    Author = "Fyodor Dostoyevsky",
+                    Price = 190.00m,
+                    StockQuantity = 5
+                },
+                new Book
+                {
+                    Title = "The Brothers Karamazov",
+                    Author = "Fyodor Dostoyevsky",
+                    Price = 250.53m,
+                    StockQuantity = 0
+                },
+                new Book
+                {
+                    Title = "The Idiots",
+                    Author = "Fyodor Dostoyevsky",
+                    Price = 166.90m,
+                    StockQuantity = 5
+                },
+                new Book
+                {
+                    Title = "Laws of Human Nature",
+                    Author = "Robert Greene",
+                    Price = 135.50m,
+                    StockQuantity = 0
+                }, 
+                new Book
+                {
+                    Title = "48 Laws of Power",
+                    Author = "Robert Greene",
+                    Price = 180.00m,
+                    StockQuantity = 10
+                },
+                new Book
+                {
+                    Title = "Art of Seduction",
+                    Author = "Robert Greene",
+                    Price = 148.00m,
+                    StockQuantity = 10
+                },
+                new Book
+                {
+                    Title = "1984",
+                    Author = "George Orwell",
+                    Price = 200.50m,
+                    StockQuantity = 7
+                },
+                new Book
+                {
+                    Title = "Animal Farm",
+                    Author = "George Orwell",
+                    Price = 178.40m,
+                    StockQuantity = 7
+                },
+                new Book
+                {
+                    Title = "The Gulag Archipelago",
+                    Author = "A7leksandr Solzhenitsyn",
+                    Price = 234.98m,
+                    StockQuantity = 4
+                }
+            };
+
+            Book.ViewAllBooks(books);
+            Book.ViewBooksByAuthor(books, "Robert Greene");
+            Book.ViewBooksByPrice(books, 120.00m, 190.00m);
+            Book.ViewBooksOutOfStock(books);
+        }
+    }
+}
